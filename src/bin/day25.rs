2c4575da1e29a5bc448fdcd6ae367d1adf994c92
Caleb Stanford <caleb.pirsquared@gmail.com>
2020-12-24 13:34:57 -0500

    day 25 placeholder

---
+++ src/bin/day25.rs
@@ -0,0 +1,18 @@
+/*
+    Advent of Code 2020
+    Caleb Stanford
+    Day 25 Solution
+    2020-12-25
+
+    Time (--release):
+*/
+
+use aoc2020::util::file_to_vec;
+
+fn main() {
+    let _input = file_to_vec("input/day25.txt");
+
+    println!("===== Part 1 =====");
+
+    println!("===== Part 2 =====");
+}
